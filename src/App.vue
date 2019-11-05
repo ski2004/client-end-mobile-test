516b41e7a5ebd9708d1be3f32cf691cd05851565
ski2004 <daneil@mail.js-tech.tw>
2019-11-05 18:50:23 +0800

    test 監聽 關閉瀏覽器或重整

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,61 @@
   </div>
 </template>
 
+<script>
+import { log } from "util";
+
+export default {
+  name: "",
+  props: {},
+  components: {},
+  data: function() {
+    // 資料
+    return {};
+  },
+  watch: {
+    //監聽值
+  },
+  computed: {
+    //相依的資料改變時才做計算方法
+  },
+  methods: {
+    // 初始
+  },
+  //BEGIN--生命週期
+  beforeCreate: function() {
+    //實體初始化
+  },
+  created: function() {
+    //實體建立完成。資料 $data 已可取得，但 $el 屬性還未被建立。
+    console.log(12132);
+    window.onbeforeunload = function(e) {
+      e = e || window.event;
+      console.log(9999999)
+      // Chrome, Safari, Firefox 4+, Opera 12+ , IE 9+
+      return false ;
+    };
+  },
+  beforeMount: function() {
+    //執行元素掛載之前。
+  },
+  mounted: function() {
+    //元素已掛載， $el 被建立。
+  },
+  beforeUpdate: function() {
+    //當資料變化時被呼叫，還不會描繪 View。
+  },
+  updated: function() {
+    //當資料變化時被呼叫，還不會描繪 View。
+  },
+  beforeDestroy: function() {
+    //實體還可使用。
+  },
+  destroyed: function() {
+    //實體銷毀。
+  }
+  //END--生命週期
+};
+</script>
 
 <style lang="scss">
 // @import '@/assets/css/all.css';

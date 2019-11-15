db29931151480aad75094f9f32c4d14349e1b495
ski2004 <daneil@mail.js-tech.tw>
2019-11-15 16:47:28 +0800

    showdoc

--- showdoc_api.sh
+++ showdoc_api.sh
@@ -12,35 +12,72 @@ url="https://www.showdoc.cc/server/api/item/updateByApi"
 cat_name="test/"
 #頁面名稱
 page_title="test"
+#指定資料夾
+folder_name="$1"
 #指定檔名
-curren_file_name="$2"
+file_name="$2"
+#測試模式
+test="false"
+
+#內文用全域變數
+doc_content=""
+doc_type=""
+doc_tables=""
+doc_example=""
+doc_desc=""
+
 # 如果第一个参数是目录，则使用参数目录。若无，则使用脚本所在的目录。
-if [[ -z "$1" ]] || [[ ! -d "$1" ]] ; then #目录判断，如果$1不是目录或者是空，则使用当前目录
+if [[ -z "$folder_name" ]] || [[ ! -d "$folder_name" ]] ; then #目录判断，如果$folder_name不是目录或者是空，则使用当前目录
 	echo "請重新輸入目錄 ... " 
-	# read -s -n1 -p "請重新輸入目錄 ... " # 本機測試
+	if	[[ $test == "true" ]]; then
+		read -s -n1 -p "按任意键继续 ... " # 本機測試打開
+	fi
 	exit
 else
-	curren_dir=$(cd $1; pwd)
+	# curren_dir=$(cd $folder_name; pwd)
+	curren_dir=$(find $folder_name -iname '*.vue' -type f )
 fi
-echo "搜尋資料夾 >>>>> $curren_dir"
+echo -e "\033[36m 搜尋資料夾:$folder_name \033[0m "
+
+# 重置
+reset(){
+	doc_type=""
+	doc_tables=""
+	doc_example=""
+	doc_desc=""
+}
+# 組合資料
+combination(){
+	# echo "combination"
+	if [[ "$doc_example" != "" ]]; then
+		doc_content+="\n####範例:\n"
+		doc_content+='```\n'
+		doc_content+="$doc_example"
+		doc_content+='```'
+	fi
+	if [[ "$doc_tables" != "" ]]; then
+		doc_content+="\n####類型:$doc_type\n"
+		doc_content+="$doc_tables \n\n"
+	fi
+}
 
 # 递归搜索文件
 searchfile() {
 
-
 	old_IFS="$IFS"
 	IFS=$'\n'            #IFS修改
-	for chkfile in $1/*
+	for chkfile in $curren_dir
 	do
-		tables_type=""
-		tables=""
-		page_content=""
-		#檔案判斷是否有符合指定
-		if [[ $chkfile =~ "$curren_file_name" ]] ; then
+		#換新檔案就重置
+		doc_content=""
+		reset
+
+		#指定檔案時判斷是否有符合
+		if [[ $chkfile =~ "$file_name" ]] ; then
 			echo "$chkfile 掃描中..."
 		else
 			echo "$chkfile 跳過..."
-			break
+			continue
 		fi
 		filesize=`ls -l $chkfile | awk '{ print $5 }'`
 		maxsize=$((1024*1024*1))  # 1M以下的文本文件才会被扫描
@@ -48,14 +85,12 @@ searchfile() {
 			# echo "正在扫描 $chkfile"
 			result=$(sed -n -e '/\/\*\*/,/\*\//p' $chkfile | grep showdoc) # 正则匹配
 		if [ ! -z "$result" ] ; then 
-					echo "$chkfile 擷取到資料"
+					echo -e "\033[33m 擷取到資料.. \033[0m"
 					txt=$(sed -n -e '/\/\*\*/,/\*\//p' $chkfile)
 					# echo "sed -n -e '/\/\*\*/,/\*\//p' $chkfile"
 					# echo "txt>>>$txt"
 					for row in $txt;do
 						# 找出字段
-						# echo "row='$row'";
-
 						finds1=$(echo $row | egrep -e '\* @')
 						finds2=$(echo $row | egrep -e '\* >|\* -')
 						if [[ "$finds1" != "" ]] || [[ "$finds2" != "" ]]; then
@@ -66,18 +101,12 @@ searchfile() {
 							# 開始
 							if [[ $data =~ "showdoc" ]];
 							then
-								# echo "-----START-----"
-								if [[ "$tables" != "" ]]; then
-									page_content+="$tables_type\n$tables \n\n"
-								fi
-								tables_type=""
-								tables=""
-							# elif [[ $data =~ "*/" ]];#結束
+								combination
+								reset
+
+							# 結束
+							# elif [[ $data =~ "*/" ]];
 							# then
-							# 	echo "-----END-----"
-							# 	if [[ "$tables" != "" ]]; then
-							# 		page_content+="$tables_type\n$tables \n\n"
-							# 	fi
 
 							# 左側層級
 							elif [[ $data =~ "* @catalog" ]];
@@ -88,66 +117,75 @@ searchfile() {
 							elif [[ $data =~ "* @title" ]];
 							then
 								page_title=${data/"* @title"/}
-
-							# 參數表
-							elif [[ $data =~ "* @param" ]];
-							then
-								if [[ "$tables" == "" ]]; then
-									tables="參數|類型|描述\n---|\n"
-								fi
-								temp=$( eval echo "${data/"* @param "/}" )
-								tables+="${temp// /|} \n"
 							
 							# 方法名稱
 							elif [[ $data =~ "* @name" ]];
 							then
-								page_content+="\n\nfunction ${data/"* @name "/} \n--\n"
-							
+								doc_content+="\n\nfunction ${data/"* @name "/} \n--\n"
+							# 位置
+							elif [[ $data =~ "* @path" ]];
+							then
+								doc_content+="\n####位置：\n"
+								doc_content+="- ${data/"* @path "/} \n\n"
 							# 類型
 							elif [[ $data =~ "* @type" ]];
 							then
-								tables_type="\n####類型:${data/"* @type "/}"
+								doc_type="${data/"* @type "/}"
+							
+							# 參數表
+							elif [[ $data =~ "* @param" ]];
+							then
+								if [[ "$doc_tables" == "" ]]; then
+									doc_tables="參數|類型|描述\n---|\n"
+								fi
+								temp=$( eval echo "${data/"* @param "/}" )
+								doc_tables+="${temp// /|} \n"
 							
 							# 描述
 							elif [[ $data =~ "* @description" ]];
 							then
-								desc=${data/"* @description "/}
-								page_content+="\n####描述：\n- $desc \n"
+								doc_content+="\n####描述：\n"
 							elif [[ $data =~ "* -" ]];
 							then
 								temp=$( eval echo "${data/"* "/}" )
-								page_content+="$temp\n"
-							
-							# 位置
-							elif [[ $data =~ "* @path" ]];
-							then
-								page_content+="\n####位置：\n- ${data/"* @path "/} \n\n"
-							
+								doc_content+="$temp\n"
+
 							# 範例
 							elif [[ $data =~ "* @example" ]];
 							then
-								page_content+="\n####範例:\n"
+								doc_example=''
 							elif [[ $data =~ "* >" ]];
 							then
-								page_content+="${data/"* >"/}\n"
+								doc_example+="${data/"* >"/}\n"
+							
+							#其他未定義
 							else
 								echo "未定義:$data"
 							fi
 						fi
 					done
-					if [[ "$tables" != "" ]]; then
-						page_content+="$tables_type\n$tables \n\n"
-					fi
-					# echo -e "結果:\n\n$page_content"
+
+					#組合內容
+					combination
 
 					# CURL begin
 					if  [[ $txt =~ "@showdoc" ]] && [[ $txt =~ "@title" ]]; then
-						echo -e "\033[32m $chkfile 扫描到内容 , 正在生成文档 \033[0m "
+						echo -e "\033[32m ------文檔API開始------ \033[0m "
+						
+						if	[[ $test == "true" ]]; then
+							echo -e "結果:\n\n$doc_content"
+						else
 						# 通过接口生成文档
 curl -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'  "${url}" --data-binary @- <<CURL_DATA
-api_key=${api_key}&api_token=${api_token}&cat_name=${cat_name}&page_title=${page_title}&page_content=$(echo -e $page_content)&s_number=$(date +%s)
+api_key=${api_key}&api_token=${api_token}&cat_name=${cat_name}&page_title=${page_title}&page_content=$(echo -e $doc_content)&s_number=$(date +%s)
 CURL_DATA
+							echo ""
+						fi
+						
+						echo -e "\033[32m ------文檔API結束------ \033[0m "
 					fi
+			else
+				echo -e "\033[33m 無資料.. \033[0m"
 			fi
 		fi
 
@@ -168,7 +206,9 @@ searchfile $curren_dir
 #
 sys=$(uname)
 if [[ $sys =~ "MS"  ]] || [[ $sys =~ "MINGW"  ]] || [[ $sys =~ "win"  ]] ; then 
-	echo "已更新文件 ..." 
-	# read -s -n1 -p "按任意键继续 ... " # 本機測試
+	echo -e "\033[33m 已更新文件.. \033[0m"
+	if	[[ $test == "true" ]]; then
+		read -s -n1 -p "按任意键继续 ... " # 本機測試打開
+	fi
 fi
 
